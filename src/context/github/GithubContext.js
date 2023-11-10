376cd6862dbc42f1055ba9d7386ea46d1b3e52a1
Isaac Orraiz <isaacorraiz@gmail.com>
2023-11-10 11:25:25 -0500

    Finished

--- src/context/github/GithubContext.js
+++ src/context/github/GithubContext.js
@@ -10,6 +10,7 @@ export const GithubProvider = ({ children }) => {
   const initialState = {
     users: [],
     user: {},
+    repos: [],
     loading: false,
   };
 
@@ -58,6 +59,31 @@ export const GithubProvider = ({ children }) => {
     }
   };
 
+  //Get repos from user
+  const getUserRepos = async (login) => {
+    setLoading();
+
+    const params = new URLSearchParams({
+      sort: "created",
+      per_page: 10,
+    });
+
+    const response = await fetch(
+      `${GITHUB_URL}/users/${login}/repos?${params}`,
+      {
+        headers: {
+          Authorization: `token ${GITHUB_TOKEN}`,
+        },
+      }
+    );
+    const data = await response.json();
+
+    dispatch({
+      type: "GET_REPOS",
+      payload: data,
+    });
+  };
+
   //Clears the users
   const clearUsers = () => {
     dispatch({
@@ -78,9 +104,11 @@ export const GithubProvider = ({ children }) => {
         users: state.users,
         user: state.user,
         loading: state.loading,
+        repos: state.repos,
         searchUsers,
         getUser,
         clearUsers,
+        getUserRepos,
       }}
     >
       {children}
